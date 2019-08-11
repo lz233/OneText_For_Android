6f1b9c8fe8091da639a1aa75c819c86677022f9e
lz233 <lz23333@outlook.com>
2019-08-11 21:52:33 +0800

    test

--- app/src/main/java/com/lz233/onetext/AboutActivity.java
+++ app/src/main/java/com/lz233/onetext/AboutActivity.java
@@ -7,14 +7,21 @@ import android.widget.TextView;
 
 import com.lz233.onetext.tools.App;
 
+import io.noties.markwon.Markwon;
+
 public class AboutActivity extends BaseActivity {
     private TextView ver_textview;
+    private TextView about_textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         //fb
         ver_textview = findViewById(R.id.ver_textview);
+        about_textview = findViewById(R.id.about_textview);
+        final Markwon markwon = Markwon.create(getApplicationContext());
         ver_textview.setText(App.getAppVersionName(AboutActivity.this)+" ("+String.valueOf(App.getAppVersionCode(AboutActivity.this)+")"));
+        markwon.setMarkdown(about_textview, getString(R.string.about_page_introduction));
+
     }
 }
