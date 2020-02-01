723b87b9282dab656ba77e5a6b3cf62dd2c628ca
lz233 <lz23333@outlook.com>
2020-02-01 18:22:26 +0800

    1.5 新增 完全重写的现代化订阅源管理器 [测试版] 新增 可选小部件阴影 完善 开源页 改善 翻译 改善 代码风格和质量

---
+++ translate.bat
@@ -0,0 +1,3 @@
+python -m opencc -c s2t -i app\src\main\res\values-zh\strings.xml -o app\src\main\res\values-zh-rMO\strings.xml
+python -m opencc -c s2hk -i app\src\main\res\values-zh\strings.xml -o app\src\main\res\values-zh-rHK\strings.xml
+python -m opencc -c s2twp -i app\src\main\res\values-zh\strings.xml -o app\src\main\res\values-zh-rTW\strings.xml
