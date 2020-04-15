7d75a2f094ece96177df6602cbb364af877e8d66
lz233 <lz23333@outlook.com>
2020-04-15 14:44:49 +0800

    test appcenter

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,4 +1,5 @@
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     export name=$(find $APPCENTER_OUTPUT_DIRECTORY -name '*.apk')
-    curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -X POST -F chat_id="$CHANNEL_ID" -F document="@$name"
+    curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -X POST -F chat_id="$SMCI_CHANNEL_ID" -F document="@$name"
+    curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -X POST -F chat_id="$ONETEXT_CHANNEL_ID" -F document="@$name"
 fi
