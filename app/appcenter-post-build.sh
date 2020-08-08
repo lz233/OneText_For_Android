dfeb1974bb29279ae7c7d4e0867c8d32a7849ff3
lz233 <lz23333@outlook.com>
2020-08-08 17:53:42 +0800

    更新 appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,5 +1,4 @@
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     export name=$(find $APPCENTER_OUTPUT_DIRECTORY -name '*.apk')
-    curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -X POST -F chat_id="$SMCI_CHANNEL_ID" -F document="@$name"
     curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -X POST -F chat_id="$ONETEXT_CHANNEL_ID" -F document="@$name"
 fi
