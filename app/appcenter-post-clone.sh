378fb16531f8bd08c714f1df732646ad9b7d5c8f
lz差不多是条咸鱼了 <lz233.sakura@gmail.com>
2020-09-26 00:18:11 +0800

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,1 +1,1 @@
-curl -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d 'chat_id=$ONETEXT_CHANNEL_ID&text=Starting build $APPCENTER_BUILD_ID%0AVariant $APPCENTER_ANDROID_VARIANT'
+curl -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=$ONETEXT_CHANNEL_ID&text=Starting build $APPCENTER_BUILD_ID%0AVariant $APPCENTER_ANDROID_VARIANT"
