bbc0a5aa0b45d6b4216037c66b6a64f58c8c0c6a
lz差不多是条咸鱼了 <lz233.sakura@gmail.com>
2020-09-26 00:25:20 +0800

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,3 +1,4 @@
+curl -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=$ONETEXT_CHANNEL_ID&text=Finishing build $APPCENTER_BUILD_ID%0AVariant $APPCENTER_ANDROID_VARIANT"
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     export name=$(find $APPCENTER_OUTPUT_DIRECTORY -name '*.apk')
     curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -X POST -F chat_id="$ONETEXT_CHANNEL_ID" -F document="@$name"
