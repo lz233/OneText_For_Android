e0f9b976ccc95ab0f879e7352832fcac5d8896f8
lz233 <lz23333@outlook.com>
2021-07-22 20:33:36 +0800

    use ezxhelper

--- app/src/main/java/com/lz233/onetext/hook/InitHook.kt
+++ app/src/main/java/com/lz233/onetext/hook/InitHook.kt
@@ -1,22 +1,49 @@
 package com.lz233.onetext.hook
 
 import android.app.Activity
-import android.app.Application
-import android.content.Context
 import android.widget.TextView
+import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
+import com.github.kyuubiran.ezxhelper.utils.findMethodByCondition
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.lz233.onetext.BuildConfig
 import com.lz233.onetext.hook.utils.HookContext
 import com.lz233.onetext.hook.utils.LogUtil
 import com.lz233.onetext.hook.utils.ktx.*
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class InitHook : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (lpparam.packageName == "com.netease.cloudmusic") {
-            XposedHelpers.findAndHookMethod(Application::class.java, "attach", Context::class.java, object : XC_MethodHook() {
+            EzXHelperInit.initHandleLoadPackage(lpparam)
+            findMethodByCondition("com.netease.cloudmusic.activity.LyricShareActivity") {
+                it.parameterTypes.size == 1 && it.parameterTypes[0].name == "com.netease.cloudmusic.meta.CommonLyricLine"
+            }.hookBefore {
+                val thisObject = it.thisObject as Activity
+                (thisObject.getObjectField("W") as TextView).setOnLongClickListener {
+                    val lyricShareActivity = "com.netease.cloudmusic.activity.LyricShareActivity".findClass()
+                    val musicInfo = lyricShareActivity.callStaticMethod("I3", thisObject)!!
+                    val list = lyricShareActivity.callStaticMethod("H3", thisObject)
+                            ?.callMethod("getRealAdapter")
+                            ?.callMethod("getList")!! as List<Any>
+                    thisObject.startActivity(thisObject.packageManager.getLaunchIntentForPackage(BuildConfig.APPLICATION_ID)?.apply {
+                        putExtra("text", StringBuilder().apply {
+                            for (commonLyricLine in list.listIterator()) {
+                                if (commonLyricLine.callMethod("isShare") as Boolean) {
+                                    append("${commonLyricLine.callMethod("getContent")}\n")
+                                }
+                            }
+                            deleteAt(length - 1)
+                        }.toString())
+                        putExtra("by", musicInfo.callMethod("getSingerName", true) as String)
+                        putExtra("from", musicInfo.callMethod("getMusicName") as String)
+                        putExtra("uri", "https://y.music.163.com/m/song?id=${musicInfo.callMethod("getId") as Long}")
+                    })
+                    true
+                }
+            }
+            /*XposedHelpers.findAndHookMethod(Application::class.java, "attach", Context::class.java, object : XC_MethodHook() {
                 override fun afterHookedMethod(param: MethodHookParam) {
                     super.afterHookedMethod(param)
                     HookContext.context = param.args[0] as Context
@@ -24,7 +51,7 @@ class InitHook : IXposedHookLoadPackage {
                     HookContext.classLoader = HookContext.context.classLoader
                     init()
                 }
-            })
+            })*/
         }
     }
 
@@ -36,7 +63,7 @@ class InitHook : IXposedHookLoadPackage {
                     HookContext.activity = activityParam.result as Activity
                     LogUtil.d("Current activity: ${HookContext.activity.javaClass}")
                 }
-        "com.netease.cloudmusic.activity.LyricShareActivity"
+        /*"com.netease.cloudmusic.activity.LyricShareActivity"
                 .hookBeforeMethod("S3", "com.netease.cloudmusic.meta.CommonLyricLine") {
                     val thisObject = it.thisObject as Activity
                     (thisObject.getObjectField("W") as TextView).setOnLongClickListener {
@@ -60,6 +87,6 @@ class InitHook : IXposedHookLoadPackage {
                         })
                         true
                     }
-                }
+                }*/
     }
 }
