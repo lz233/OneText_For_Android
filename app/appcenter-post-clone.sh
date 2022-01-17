7aa53645dbc759ecd5728395b611e89ad03e7377
lz233 <lz23333@outlook.com>
2022-01-17 21:34:31 +0800

    test

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,4 +1,4 @@
 PRE_BUILD_MESSAGE_RESULT=$(curl -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=$ONETEXT_CHANNEL_ID&parse_mode=MarkdownV2&text=*🚀 Build $APPCENTER_BUILD_ID Starting*%0A%0A*Variant* $APPCENTER_ANDROID_VARIANT")
 export PRE_BUILD_MESSAGE_ID=$(echo ${PRE_BUILD_MESSAGE_RESULT} | jq '.result.message_id')
 echo $PRE_BUILD_MESSAGE_ID >PRE_BUILD_MESSAGE_ID
-curl -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=$ONETEXT_CHANNEL_ID&parse_mode=MarkdownV2&text=$(echo $JAVA_HOME)"
+curl -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=$ONETEXT_CHANNEL_ID&parse_mode=MarkdownV2&text=$(ls /Library/Java/JavaVirtualMachines)"
